5ad85efb82a1238936c3dac4b58cf7768b27a8cd
Danyalzulfiqar <danyalzulfiqar01@gmail.com>
2025-02-22 01:15:42 +0500

    Delete unnecessory files and update packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Chat from './pages/Chat';
+import NotFound from './pages/NotFound';
+import Explore from './pages/Explore';
+import Notifications from './pages/Notifications';
+import Saved from './pages/Saved';
+import Inspiration from './pages/Inspiration';
+import Create from './pages/Create';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/saved" element={<Saved />} />
+        <Route path="/inspiration" element={<Inspiration />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
