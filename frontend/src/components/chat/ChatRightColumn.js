77461d6fed328ca7f4d3742a7c3d78b4014f62d9
Danyalzulfiqar <danyalzulfiqar01@gmail.com>
2025-02-22 01:14:45 +0500

    add right coloumn component

---
+++ frontend/src/components/chat/ChatRightColumn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChatRightColumn() {
+  return (
+    <div className="flex h-full min-h-[36rem] flex-1 flex-col items-center justify-center p-7">
+      <h2 className="text-xl font-semibold text-gray-600">Coming Soon</h2>
+    </div>
+  );
+}
+
+export default ChatRightColumn; 
